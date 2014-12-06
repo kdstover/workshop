207021e3536cd4aa8bf70eb21c16293452eb9466
Kerri Stover <kdstover@gmail.com>
2014-12-06 14:32:36 -0500

    Added hello world file - because this is a workshop.

---
+++ hello_world.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+    <?php
+        $var1=10;
+        echo $var1;
+        echo "<br />";
+        $var2 = "Hello World!";
+        echo $var2;
+    ?>
+    <p>This is an example of a file change.</p>
+    </body>
+</html>
